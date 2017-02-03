d3c553430604aefcebaf49b98382027cabecfde7
css94 <carlotasanchezsansegundo@gmail.com>
2017-02-03 20:42:27 +0100

    new

---
+++ src/main/java/conexionssh/Patologia.java
@@ -0,0 +1,24 @@
+package conexionssh;
+
+public class Patologia {
+	private int id;
+	private String nombre;
+	
+	public Patologia(int id, String nombre) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
